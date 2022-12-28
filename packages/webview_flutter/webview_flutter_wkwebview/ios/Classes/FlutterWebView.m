bbedb138bd681e2f0ea2b95993280f94151cec96
DoLT <dolt@guide.inc>
2022-12-28 12:43:14 +0700

    feat(webview_flutter,ios): support for js alert, confirm, prompt

--- packages/webview_flutter/webview_flutter_wkwebview/ios/Classes/FlutterWebView.m
+++ packages/webview_flutter/webview_flutter_wkwebview/ios/Classes/FlutterWebView.m
@@ -104,6 +104,11 @@
 }
 @end
 
+@interface FLTWebViewController () <WKUIDelegate> {
+  UIViewController* _viewController;
+}
+@end
+
 @implementation FLTWebViewController {
   FLTWKWebView *_webView;
   int64_t _viewId;
@@ -187,6 +192,12 @@
         [_webView loadRequest:request];
       }
     }
+
+    _viewController = [UIApplication sharedApplication].delegate.window.rootViewController;
+    UIViewController* presentedViewController = _viewController.presentedViewController;
+    UIViewController* currentViewController =
+        presentedViewController != nil ? presentedViewController : _viewController;
+    [currentViewController.view addSubview:_webView];
   }
   return self;
 }
@@ -737,6 +748,79 @@
   }
 
   return nil;
+}
+
+- (void)webView:(WKWebView*)webView
+    runJavaScriptAlertPanelWithMessage:(NSString*)message
+                      initiatedByFrame:(WKFrameInfo*)frame
+                     completionHandler:(void (^)(void))completionHandler {
+  UIAlertController* alert =
+      [UIAlertController alertControllerWithTitle:nil
+                                          message:message
+                                   preferredStyle:UIAlertControllerStyleAlert];
+
+  [alert addAction:[UIAlertAction actionWithTitle:NSLocalizedString(@"OK", nil)
+                                            style:UIAlertActionStyleCancel
+                                          handler:^(UIAlertAction* action) {
+                                            completionHandler();
+                                          }]];
+
+  [_viewController presentViewController:alert animated:YES completion:nil];
+}
+
+- (void)webView:(WKWebView*)webView
+    runJavaScriptConfirmPanelWithMessage:(NSString*)message
+                        initiatedByFrame:(WKFrameInfo*)frame
+                       completionHandler:(void (^)(BOOL result))completionHandler {
+  UIAlertController* alert =
+      [UIAlertController alertControllerWithTitle:nil
+                                          message:message
+                                   preferredStyle:UIAlertControllerStyleAlert];
+
+  [alert addAction:[UIAlertAction actionWithTitle:NSLocalizedString(@"Cancel", nil)
+                                            style:UIAlertActionStyleCancel
+                                          handler:^(UIAlertAction* action) {
+                                            completionHandler(NO);
+                                          }]];
+
+  [alert addAction:[UIAlertAction actionWithTitle:NSLocalizedString(@"OK", nil)
+                                            style:UIAlertActionStyleDefault
+                                          handler:^(UIAlertAction* action) {
+                                            completionHandler(YES);
+                                          }]];
+
+  [_viewController presentViewController:alert animated:YES completion:nil];
+}
+
+- (void)webView:(WKWebView*)webView
+    runJavaScriptTextInputPanelWithPrompt:(NSString*)prompt
+                              defaultText:(NSString*)defaultText
+                         initiatedByFrame:(WKFrameInfo*)frame
+                        completionHandler:(void (^)(NSString* result))completionHandler {
+  UIAlertController* alert =
+      [UIAlertController alertControllerWithTitle:nil
+                                          message:prompt
+                                   preferredStyle:UIAlertControllerStyleAlert];
+
+  [alert addTextFieldWithConfigurationHandler:^(UITextField* textField) {
+    textField.placeholder = prompt;
+    textField.secureTextEntry = NO;
+    textField.text = defaultText;
+  }];
+
+  [alert addAction:[UIAlertAction actionWithTitle:NSLocalizedString(@"Cancel", nil)
+                                            style:UIAlertActionStyleCancel
+                                          handler:^(UIAlertAction* action) {
+                                            completionHandler(nil);
+                                          }]];
+
+  [alert addAction:[UIAlertAction actionWithTitle:NSLocalizedString(@"OK", nil)
+                                            style:UIAlertActionStyleDefault
+                                          handler:^(UIAlertAction* action) {
+                                            completionHandler([alert.textFields.firstObject text]);
+                                          }]];
+
+  [_viewController presentViewController:alert animated:YES completion:nil];
 }
 
 @end
